f0b42a81ac3fb027e1a8b0977b2617cb8d963c5b
Rachel Collins <spiritedndiligent@gmail.com>
2022-12-20 23:28:46 -0500

    basic form structure complete

--- src/Form.js
+++ src/Form.js
@@ -10,12 +10,35 @@ State
 
 const Form = () => {
 return (
-    <div className="form">
+    <div>
         <h2>User Creation Form</h2>
-        <form>
-            <label>Full Name:</label>
-            <input type="text" className="flname" name="flname"/>
-        </form>
+        <div className="formDiv">
+            <form className="innerForm">
+                <div >
+                            <label className="column">Full Name:</label>
+                            <input type="text" id="flname" name="flname"/> 
+                        </div>
+                        <div>
+                            <label className="column">Email:</label>
+                            <input type="text" id="email" name="email"/> 
+                        </div>
+                        <div>
+                            <label className="column">Password:</label>
+                            <input type="password" id="pword" name="pword" minLength="8" required />
+                        </div>
+                        <div>
+                            <label className="column">Occupation:</label>
+                            <input type="text" id="work" name="work"/>
+                        </div>
+                        <div>
+                            <label className="column">State:</label>
+                            <input type="text" id="state" name="state"/>
+                </div>
+                        
+            </form>
+        </div>
+        
+        <button>Send</button>
     </div>
 );
 }
