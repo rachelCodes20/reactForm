238cfa40ea6514008b8a1605c28aeb02f1976b51
Rachel Collins <spiritedndiligent@gmail.com>
2022-12-21 20:46:03 -0500

    functioning usestate func for input labels

--- src/Form.js
+++ src/Form.js
@@ -5,8 +5,37 @@ Password
 Occupation
 State 
 */
-
+import React, { useState } from 'react';
 const Form = () => {
+// const [fullName, setFullName] = useState('');
+// const [email, setEmail] = useState('');
+// const [password, setpWord] = useState('');
+// const [occupation, setOccupation] = useState('');
+// const [homeState, setHomeState] = useState('');
+const [input, setInput] = useState({
+fullName: "",
+email: "",
+password: "",
+occupation: "",
+state: ""
+});
+
+// console.log('usestate', useState())
+const handleInput = (e) => {
+  //take previous obj val
+  console.log('event',e.target.value)
+  
+    const inputName = e.target.name;
+    const inputValue = e.target.value;
+    console.log('typing', e.target.value)
+    setInput((prev) => { 
+      return {
+        //return state object and update only inputVal
+      ...prev,[inputName]: inputValue
+    }
+  })
+  };
+  console.log('input',input)
   return (
     <div>
       <h2>User Creation Form</h2>
@@ -14,29 +43,32 @@ const Form = () => {
         <form className="innerForm">
           <div>
             <label className="column">Full Name:</label>
-            <input type="text" id="flname" name="flname" />
+            <input type="text" id="fullName" name="fullName" 
+            onChange={handleInput} 
+            />
           </div>
           <div>
             <label className="column">Email:</label>
-            <input type="text" id="email" name="email" />
+            <input type="email" id="email" name="email" onChange={handleInput} />
           </div>
           <div>
             <label className="column">Password:</label>
             <input
               type="password"
-              id="pword"
-              name="pword"
+              id="password"
+              name="password"
+              onChange={handleInput} 
               minLength="8"
               required
             />
           </div>
           <div>
             <label className="column">Occupation:</label>
-            <input type="text" id="work" name="work" />
+            <input type="text" id="work" name="occupation" onChange={handleInput} />
           </div>
           <div>
             <label className="column">State:</label>
-            <input type="text" id="state" name="state" />
+            <input type="text" id="state" name="state" onChange={handleInput} />
           </div>
         </form>
       </div>
