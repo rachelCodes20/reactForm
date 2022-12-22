e41745063d4b1b701ce7c574e98e2d408fa7087d
Rachel Collins <spiritedndiligent@gmail.com>
2022-12-21 20:53:53 -0500

    desctructing from target object

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Form from "./Form";
 const App = () => {
-// const [fullName, setFullName] = useState('');
-// const [email, setEmail] = useState('');
-// const [pWord, setpWord] = useState('');
-// const [occupation, setOccupation] = useState('');
-// const [homeState, setHomeState] = useState('');
+  // const [fullName, setFullName] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [pWord, setpWord] = useState('');
+  // const [occupation, setOccupation] = useState('');
+  // const [homeState, setHomeState] = useState('');
 
-// const handleInput = (event) => {
-//   setFullName(event.target.value);
-// }
+  // const handleInput = (event) => {
+  //   setFullName(event.target.value);
+  // }
   return (
     <div className="App">
       <h1>Hello World!</h1>
--- src/Form.js
+++ src/Form.js
@@ -5,37 +5,30 @@ Password
 Occupation
 State 
 */
-import React, { useState } from 'react';
+import React, { useState } from "react";
 const Form = () => {
-// const [fullName, setFullName] = useState('');
-// const [email, setEmail] = useState('');
-// const [password, setpWord] = useState('');
-// const [occupation, setOccupation] = useState('');
-// const [homeState, setHomeState] = useState('');
-const [input, setInput] = useState({
-fullName: "",
-email: "",
-password: "",
-occupation: "",
-state: ""
-});
+  const [input, setInput] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+    occupation: "",
+    state: "",
+  });
 
-// console.log('usestate', useState())
-const handleInput = (e) => {
-  //take previous obj val
-  console.log('event',e.target.value)
-  
-    const inputName = e.target.name;
-    const inputValue = e.target.value;
-    console.log('typing', e.target.value)
-    setInput((prev) => { 
+  const handleInput = (e) => {
+    //take previous obj val
+    console.log("event", e.target.value);
+    const { name, value } = e.target;
+    
+    setInput((prev) => {
       return {
         //return state object and update only inputVal
-      ...prev,[inputName]: inputValue
-    }
-  })
+        ...prev,
+        [name]: value,
+      };
+    });
   };
-  console.log('input',input)
+  console.log("input", input);
   return (
     <div>
       <h2>User Creation Form</h2>
@@ -43,13 +36,21 @@ const handleInput = (e) => {
         <form className="innerForm">
           <div>
             <label className="column">Full Name:</label>
-            <input type="text" id="fullName" name="fullName" 
-            onChange={handleInput} 
+            <input
+              type="text"
+              id="fullName"
+              name="fullName"
+              onChange={handleInput}
             />
           </div>
           <div>
             <label className="column">Email:</label>
-            <input type="email" id="email" name="email" onChange={handleInput} />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={handleInput}
+            />
           </div>
           <div>
             <label className="column">Password:</label>
@@ -57,14 +58,19 @@ const handleInput = (e) => {
               type="password"
               id="password"
               name="password"
-              onChange={handleInput} 
+              onChange={handleInput}
               minLength="8"
               required
             />
           </div>
           <div>
             <label className="column">Occupation:</label>
-            <input type="text" id="work" name="occupation" onChange={handleInput} />
+            <input
+              type="text"
+              id="work"
+              name="occupation"
+              onChange={handleInput}
+            />
           </div>
           <div>
             <label className="column">State:</label>
